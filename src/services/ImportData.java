e58cc88d5700ed72d4ea29d23e751786bbe4c0d9
alexis-toma.ghillis@metrosystems.net <alexis-toma.ghillis@metrosystems.net>
2017-10-29 15:48:57 +0200

    renamed main class

--- src/services/ImportData.java
+++ src/services/ImportData.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class TestSelenium {
+public class ImportData {
   public static void main(String[] args) {
     System.setProperty("webdriver.gecko.driver", "C:/APPS/geckodriver.exe");
 
